1c4b142e96c4e6d548e8227ec099795ffd982935
tmosbruger <tmosbruger@a763469b-d363-47ea-a6e5-8f617fc850fa>
2012-10-31 18:03:49 +0000

    this is a test class

---
+++ trunk/Source/edu/utah/tlm/Test.java
@@ -0,0 +1,15 @@
+package edu.utah.tlm;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hi");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
