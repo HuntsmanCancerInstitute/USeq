6678ba8701b0761658817bb535847ac2eb167e12
biotelerock <biotelerock@a763469b-d363-47ea-a6e5-8f617fc850fa>
2013-05-29 17:13:23 +0000

    Mods to the MethylationArray apps, corrected a minor bug, added non paired functionality, updated the docs.

---
+++ Source/edu/utah/seq/data/MethylationArraySamplePair.java
@@ -0,0 +1,41 @@
+package edu.utah.seq.data;
+
+import java.util.ArrayList;
+import util.gen.Num;
+
+/**Container for paired beta values 0-1 off Illumina methylation data.  Zero values are excluded.
+ * @author davidnix*/
+public class MethylationArraySamplePair{
+
+	private float[] treatment;
+	private float[] control;
+	private boolean performPairedAnalysis;
+
+	public MethylationArraySamplePair(float[] treatment, float[] control, boolean performPairedAnalysis) {
+		this.treatment = treatment;
+		this.control = control;
+		this.performPairedAnalysis = performPairedAnalysis;
+	}
+
+	public void randomize() {
+		Num.randomizePairedValues(treatment, control, System.currentTimeMillis());
+	}
+
+	/**StopIndex is included!*/
+	public void fetchScoresByIndex(int startIndex, int stopIndex, ArrayList<Float> treatmentAL, ArrayList<Float> controlAL){
+		if (performPairedAnalysis){
+			for (int i=startIndex; i<= stopIndex; i++){
+				if (treatment[i] !=0.0f && control[i] != 0.0f){
+					treatmentAL.add(treatment[i]);
+					controlAL.add(control[i]);
+				}
+			}
+		}
+		else{
+			for (int i=startIndex; i<= stopIndex; i++){
+				if (treatment[i] !=0.0f ) treatmentAL.add(treatment[i]);
+				if (control[i] != 0.0f) controlAL.add(control[i]);
+			}
+		}
+	}
+}
